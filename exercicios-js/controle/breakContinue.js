772c8899391baa7f08c1263d5a7c279bd3241e66
minafrajr <casa20211>
2018-11-05 22:05:37 -0200

    parametros1

--- exercicios-js/controle/breakContinue.js
+++ exercicios-js/controle/breakContinue.js
@@ -2,7 +2,25 @@ const notas = [1,2,3,4,5,6,7,8,9]
 
 for( let x in notas){
     if(x == 5) {
-        break
+        break // for while e switch
     }
     console.log(`${x} = ${notas[x]}`)
 }
+
+for (y in notas){
+    if(y==5){
+        continue //age em estruturas de repeticao
+    }
+    console.log(`${y} = ${notas[y]}`)
+
+}
+
+externo:
+for (a in notas){
+    for(b in notas){
+        if(a ==2 && b==3) break externo
+        console.log(`Par =  ${a}, ${b}`)
+    }
+}
+
+console.log('Fim')
