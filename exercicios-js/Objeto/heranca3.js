8a4dea6d627d0aa7e470e86e30a02d7b650ebde5
minafrajr <casa20211>
2018-11-15 21:50:16 -0200

    objetos e mais

--- exercicios-js/Objeto/heranca3.js
+++ exercicios-js/Objeto/heranca3.js
@@ -5,7 +5,7 @@ filha1.nome = 'Ana'
 console.log(filha1.corCabelo)
 
 const filha2 = Object.create(pai,
-    {nome:{valeue: 'Bia', writable: false, enumrable: true}
+    {nome:{value: 'Bia', writable: false, enumrable: true}
 })
 
 console.log(filha2.nome)
@@ -17,6 +17,5 @@ console.log(Object.keys(filha1))
 console.log(Object.keys(filha2))
 
 for(let key in filha2){
-    filha2.hasOwnProperty(key) ?
-    console.log(key): console.log(`por herança: ${key}`)
+    filha2.hasOwnProperty(key) ? console.log(key): console.log(`por herança: ${key}`)
 }
