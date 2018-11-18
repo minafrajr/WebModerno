d3d771c4c35bb5fc3cedf792e8c1c0444b6f4284
minafrajr <casa20211>
2018-11-18 21:57:56 -0200

    Objeto final

--- exercicios-js/Objeto/jsonVSObj.js
+++ exercicios-js/Objeto/jsonVSObj.js
@@ -1,8 +1,15 @@
+//JSON -> formato textual para transferência de dados
+
 const obj = {a:1, b: 2, c: 3, soma(){return a + b + c}}
 
-console.log(JSON.stringify(obj))
+console.log(JSON.stringify(obj))//transforma o objeto em formato JSON
 
-//console.log(JSON.parse("{ a:1 b: 2, c: 3}"))
-//console.log(JSON.parse("{ 'a':1 'b': 2, 'c': 3}"))
-console.log(JSON.parse('{ "a":1 "b": 2, "c": 3}'))
-console.log(JSON.parse('{"a": 1.78 , "b": "string", "c": true, "d": {}, "e":[] }'))
+//console.log(JSON.parse("{ a:1 b: 2, c: 3}")) //atributos deve vir com aspas duplas
+//console.log(JSON.parse("{ 'a':1 'b': 2, 'c': 3}")) //atributos deve vir com aspas duplas
+console.log(JSON.parse('{ "a":1, "b": 2, "c": 3}')) //correto
+console.log(JSON.parse('{"a": 1.78 , "b": "string", "c": true, "d": {}, "e":[] }')) //correto
+
+const Pessoa = JSON.parse('{ "nome": "Carlos", "idade": 15 }')
+
+console.log(Pessoa.nome)
+console.log(Pessoa.idade)
