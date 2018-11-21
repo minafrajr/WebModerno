984eb3e90645ebf35ceef88c78f86199a643013a
minafrajr <casa20211>
2018-11-21 21:23:37 -0200

    fim do array

--- exercicios-js/array/flatMap.js
+++ exercicios-js/array/flatMap.js
@@ -24,3 +24,11 @@ const getNotaDoAluno = aluno => aluno.nota
 const getNotasDaTurma = turma => turma.alunos.map(getNotaDoAluno)
 
 const notas1 = escola.map(getNotasDaTurma)
+console.log(notas1)
+
+Array.prototype.flatMap = function(callback){
+    return Array.prototype.concat.apply([], this.map(callback))
+}
+
+const notas2 = escola.flatMap(getNotasDaTurma)
+console.log(notas2)
