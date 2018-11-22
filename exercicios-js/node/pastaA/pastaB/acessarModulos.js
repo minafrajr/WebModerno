794e4335fd6e9d88f0a36baac669dd485e24805f
minafrajr <casa20211>
2018-11-21 23:09:51 -0200

    iniciando node

---
+++ exercicios-js/node/pastaA/pastaB/acessarModulos.js
@@ -0,0 +1,13 @@
+const moduloA = require('../../moduloA') //case insentive exceto para linux
+//const moduloA = require('E:\\CursoWeb\\WebModerno\\exercicios-js\\node\\moduloA.js')
+console.log(moduloA.ola)
+
+const c = require('./pastaC')
+console.log(c.ola2)
+
+/* const http = require('http')
+http.createServer((req, res)=>{
+    res.write('Bom dia!')
+    res.end()
+}).listen(8080) */
+
