5f7866693b6b995469b86a9bded95bf66548a35b
minafrajr <casa20211>
2018-11-27 22:26:12 -0200

    final node 2

---
+++ exercicios-js/node/temporizador.js
@@ -0,0 +1,21 @@
+const schedule = require('node-schedule')
+
+const tarefa1 = schedule.scheduleJob('*/1 * 22 * * 2', function() {
+    console.log('Executando Tarefa 1!', new Date().getMinutes(),new Date().getSeconds())
+})
+
+setTimeout(function(){
+    tarefa1.cancel()
+    console.log('Cancelando Tarefa1')
+},20000)
+
+//setImmediate ou SetInterval - temporizadores nativos
+
+const regra = new schedule.RecurrenceRule()
+regra.dayOfWeek = [new schedule.Range(1,5)]
+regra.hour = 22
+regra.second = 30
+
+const tarefa2 = schedule.scheduleJob(regra, function(){
+    console.log('Executando Tarefa 2!', new Date().getSeconds())
+})
