367376e95d534df5324e2f460d36f04323715929
Carlos A. Minafra Jr <carlos.minafrajr@gmail.com>
2019-06-30 13:06:27 -0300

    gulp

--- exercicios-WEB/gulp/basico/gulpfile.js
+++ exercicios-WEB/gulp/basico/gulpfile.js
@@ -1,6 +1,22 @@
 const gulp = require('gulp')
 
 gulp.task('default', () => {
-	console.log('OK!')
-	//gulp.start('copiar','fim')
+	//console.log('OK!')
+	gulp.start('copiar', 'fim')
+})
+
+gulp.task('copiar', ['antes1', 'antes2'], () => {
+	console.log('Copiar!!!!!')
+})
+
+gulp.task('antes1', () => {
+	console.log('Antes 1!!!!!')
+})
+
+gulp.task('antes2', () => {
+	console.log('Antes 2 !!!!!')
+})
+
+gulp.task('fim', () => {
+	console.log('fim!')
 })
