38640adcbba4dd8abed3916cb3ee792c1a71e459
minafrajr <casa20211>
2019-07-16 08:10:43 -0300

    final gulp

---
+++ exercicios-WEB/gulp/spa/gulpTasks/servidor.js
@@ -0,0 +1,20 @@
+const gulp = require('gulp')
+const watch = require('gulp-watch')
+const webserver = require('gulp-webserver')
+
+gulp.task('servidor', ['monitoraMudancas'], () => {
+  watch('src/**/*.html', () => gulp.start('app.html'))
+  watch('src/**/*.css', () => gulp.start('app.css'))
+  watch('src/**/*.js', () => gulp.start('app.js'))
+  watch('src/assets/imgs/*.*', () => gulp.start('app.imgs'))
+})
+
+gulp.task('monitoraMudancas', () => {
+  return gulp.src('build').pipe(
+    webserver({
+      livereload: true,
+      port: 8080,
+      open: true
+    })
+  )
+})
