246a6b7f72ba7d816ba2a2bea8691f9d66d4c5f2
Carlos A. Minafra Jr <carlos.minafrajr@gmail.com>
2020-12-14 19:40:45 -0300

    user crud routes

--- exercicios-WEB/react/crud/frontend/src/components/template/Logo.jsx
+++ exercicios-WEB/react/crud/frontend/src/components/template/Logo.jsx
@@ -1,11 +1,12 @@
-import './Logo.css'
-import logo from '../../assets/imgs/logo.png'
-import React from 'react'
+import './Logo.css';
+import logo from '../../assets/imgs/logo.png';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default props => (
-  <aside className="logo">
-    <a href="/" className="logo">
-      <img src={logo} alt="logo" />
-    </a>
-  </aside>
-)
+export default (props) => (
+    <aside className='logo'>
+        <Link to='/' className='logo'>
+            <img src={logo} alt='logo' />
+        </Link>
+    </aside>
+);
--- exercicios-WEB/react/crud/frontend/src/components/template/Nav.jsx
+++ exercicios-WEB/react/crud/frontend/src/components/template/Nav.jsx
@@ -1,13 +1,15 @@
-import './Nav.css'
-import React from 'react'
+import './Nav.css';
+import React from 'react';
 
-export default props =>
-    <aside className="menu-area">
-        <nav className="menu">
-        <a href="#/">
-            <i className="fa fa-home"></i> Início
-        </a>
-            <a href="#/users">
-                    <i className="fa fa-users"></i>Usuários</a>
-    </nav>
-</aside>
+export default (props) => (
+    <aside className='menu-area'>
+        <nav className='menu'>
+            <Link to='#/'>
+                <i className='fa fa-home'></i> Início
+            </Link>
+            <Link to='#/users'>
+                <i className='fa fa-users'></i>Usuários
+            </Link>
+        </nav>
+    </aside>
+);
