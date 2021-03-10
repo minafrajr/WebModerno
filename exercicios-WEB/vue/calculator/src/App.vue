cfb1f1afcc51294c149847e7838e8622ea9a4fc3
Carlos A. Minafra Jr <carlos.minafrajr@gmail.com>
2021-03-10 13:23:05 -0300

    calculator vue

---
+++ exercicios-WEB/vue/calculator/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+     <h1><Calculator/></h1>
+  </div>
+
+</template>
+
+<script>
+import Calculator from "./main/Calculator"
+export default {
+    components:{Calculator}
+}
+</script>
+<style>
+@font-face{
+    font-family: "RobotoMono";
+    src:url('../fonts/RobotoMono-Thin.ttf');
+}
+* {
+    font-family:"RobotoMono", monospace;
+}
+body{
+    margin: 0;
+}
+
+#app{
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+
+    color: #fff;
+    background: linear-gradient(to right, rgb(83, 105, 118), rgb(44, 46, 73));
+
+}
+
+</style>
