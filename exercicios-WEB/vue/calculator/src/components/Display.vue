0807246b971568024fa6c45a190626f170c5ed4e
Carlos A. Minafra Jr <carlos.minafrajr@gmail.com>
2021-03-28 21:07:21 -0300

    button and display

--- exercicios-WEB/vue/calculator/src/components/Display.vue
+++ exercicios-WEB/vue/calculator/src/components/Display.vue
@@ -1,13 +1,22 @@
 <template>
-  <div class="display">0</div>
+  <div class="display">{{ value }}</div>
 </template>
 
 <script>
-export default {
-
-}
+export default { prop: ['value'] };
 </script>
 
 <style>
+.display {
+  grid-column: span 4;
+  background-color: #0004;
 
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  padding: 20px;
+  font-size: 2.1rem;
+
+  overflow: hidden;
+}
 </style>
--- exercicios-WEB/vue/calculator/src/main/Calculator.vue
+++ exercicios-WEB/vue/calculator/src/main/Calculator.vue
@@ -1,28 +1,42 @@
 <template>
-<div class="calculator">
-    <Display/>
-    <Button/>
-    <Button/>
-    <Button/>
-</div>  
+  <div class="calculator">
+    <Display />
+    <Button label="AC" triple />
+    <Button label="/" operation />
+    <Button label="7" />
+    <Button label="8" />
+    <Button label="9" />
+    <Button label="*" operation />
+    <Button label="4" />
+    <Button label="5" />
+    <Button label="6" />
+    <Button label="-" operation />
+    <Button label="1" />
+    <Button label="2" />
+    <Button label="3" />
+    <Button label="+" operation />
+    <Button label="0" double />
+    <Button label="." />
+    <Button label="=" operation />
+  </div>
 </template>
 
 <script>
-import Display from "../components/Display"
-import Button from "../components/Button"
+import Display from '../components/Display';
+import Button from '../components/Button';
 export default {
-components: {Button, Display}
-}
+  components: { Button, Display },
+};
 </script>
 
 <style>
-.calculator{
-    height: 320px;
-    width: 235;
-border-radius: 5px;
-overflow: hidden;
-display: grid;
-grid-template-columns: repeat(4,25%);
-grid-auto-rows: 1fr 48px 48px 48px 48px 48px ;
+.calculator {
+  height: 320px;
+  width: 235;
+  border-radius: 5px;
+  overflow: hidden;
+  display: grid;
+  grid-template-columns: repeat(4, 25%);
+  grid-auto-rows: 1fr 48px 48px 48px 48px 48px;
 }
 </style>
