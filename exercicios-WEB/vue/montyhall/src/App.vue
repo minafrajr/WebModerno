95487f7710325190a43827269185f8c58897bec6
minafrajr <carlos.minafrajr@gmail.com>
2021-04-02 12:29:56 -0300

    final Monty Hall and init Angular

--- exercicios-WEB/vue/montyhall/src/App.vue
+++ exercicios-WEB/vue/montyhall/src/App.vue
@@ -1,14 +1,51 @@
 <template>
 	<div id="app">
 		<h1>Monty Hall Door Problem</h1>
-		<Door number="1" :hasGift="false" />
+		<div class="form">
+			<div v-if="!started">
+				<label for="doorsAmount">Número de portas:</label>
+				<input
+					type="text"
+					id="doorsAmount"
+					size="3"
+					v-model.number="doorsAmount"
+				/>
+			</div>
+			<div v-if="!started">
+				<label for="selectedDoor">Porta a ser premiada:</label>
+				<input
+					type="text"
+					id="selectedDoor"
+					size="3"
+					v-model.number="selectedDoor"
+				/>
+			</div>
+			<button v-if="!started" @click="started = true">Iniciar</button>
+			<button v-if="started" @click="started = false">Reiniciar</button>
+		</div>
+		<!-- fim form -->
+		<div class="doors" v-if="started">
+			<div v-for="i in doorsAmount" :key="i">
+				<Door :hasGift="i == selectedDoor" :number="i" />
+			</div>
+		</div>
 	</div>
 </template>
 
 <script>
 import Door from './components/Door';
 
-export default { name: 'App', components: { Door } };
+export default {
+	name: 'App',
+	components: { Door },
+	data: function() {
+		return {
+			started: false,
+			doorsAmount: 3,
+			selectedDoor: null,
+		};
+	},
+};
 </script>
 
 <style>
@@ -35,5 +72,25 @@ body {
 	background-color: #0004;
 	padding: 20px;
 	margin-bottom: 60px;
+}
+.form {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	margin-bottom: 40px;
+}
+
+.form,
+.form input,
+.form button {
+	margin-bottom: 10px;
+	font-size: 1.5rem;
+}
+.doors {
+	align-self: stretch;
+	display: flex;
+	justify-content: space-around;
+	flex-wrap: wrap;
 }
 </style>
