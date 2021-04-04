d300a9df523477ea3169adbc8a5f8a4281e20fed
minafrajr <carlos.minafrajr@gmail.com>
2021-04-04 00:07:50 -0300

    angular 3

---
+++ exercicios-WEB/Angular/frontend/src/app/components/product/product-create/product-create.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { Router } from '@angular/router';
+
+@Component({
+	selector: 'app-product-create',
+	templateUrl: './product-create.component.html',
+	styleUrls: ['./product-create.component.css'],
+})
+export class ProductCreateComponent implements OnInit {
+	constructor(private productService: ProductService, private router: Router) {}
+
+	ngOnInit(): void {}
+
+	OnSave(): void {
+		this.productService.showMessage('Salvo com sucesso!');
+	}
+
+	OnCancel(): void {
+		this.router.navigate(['/products']);
+	}
+}
---
+++ exercicios-WEB/Angular/frontend/src/app/components/product/product.model.ts
@@ -0,0 +1,5 @@
+export interface Product {
+	id?: Number;
+	name: string;
+	price: number;
+}
