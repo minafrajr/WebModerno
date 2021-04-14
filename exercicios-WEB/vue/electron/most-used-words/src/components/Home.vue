854f022d8c794c9f65aace3610ebec8cb123d753
minafrajr <carlos.minafrajr@gmail.com>
2021-04-14 20:29:17 -0300

    process words

--- exercicios-WEB/vue/electron/most-used-words/src/components/Home.vue
+++ exercicios-WEB/vue/electron/most-used-words/src/components/Home.vue
@@ -2,15 +2,21 @@
 	<v-container fluid>
 		<v-form>
 			<v-file-input
+				v-model="files"
 				label="Selecione as legendas"
 				prepend-icon="mdi-message-text"
 				append-outer-icon="mdi-send"
 				outlined
 				multiple
 				chips
-				v-model="files"
 				@click:append-outer="processSubtitles"
-			/>
+			>
+				<template v-slot:selection="{ text }">
+					<v-chip small label color="primary">
+						{{ text }}
+					</v-chip>
+				</template>
+			</v-file-input>
 		</v-form>
 		<div class="pills">
 			<Pill
@@ -24,19 +30,25 @@
 </template>
 
 <script>
+import { ipcRenderer } from 'electron';
 import Pill from './Pill.vue';
-
 export default {
 	components: { Pill },
-	data: function() {
-		return {
-			files: [],
-			groupedWords: [{ name: 'house', amount: 21 }],
-		};
-	},
+	data: () => ({
+		files: [],
+		groupedWords: [{ name: 'pen', amount: 21 }],
+	}),
 	methods: {
 		processSubtitles() {
-			console.log('processSubtitle');
+			/*DOM objects etc. are not serializable. Electron 9.0 (and newer) throws "object could not be cloned" error when unserializable objects are sent.
+			 */
+			const paths = this.files.map((f) => f.path);
+
+			ipcRenderer.send('process-subtitles', paths);
+
+			ipcRenderer.on('process-subtitles', (event, resp) => {
+				this.groupedWords = resp;
+			});
 		},
 	},
 };
